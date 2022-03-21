a0e8905afb269b343ec26c7ee9a55e84c59bd560
何小有 <30341875+hekaiyou@users.noreply.github.com>
2022-03-21 15:21:10 +0800

    Create api_permission.py

---
+++ apis/users/api_permission.py
@@ -0,0 +1,25 @@
+from core.model import NoPaginate
+from .models import PermissionRead
+from fastapi import APIRouter, Depends
+from core.dependencies import get_api_routes
+
+router = APIRouter(
+    prefix='/permission',
+)
+
+
+@router.get(
+    '/',
+    response_model=NoPaginate,
+    summary='读取权限',
+)
+async def read_permission(routes: dict = Depends(get_api_routes)):
+    all_item = []
+    for path, route in routes.items():
+        all_item.append(PermissionRead(
+            name=route['name'],
+            path=path,
+            tag=route['tag'],
+            summary=route['summary'],
+        ))
+    return NoPaginate(all_item=all_item, total=len(all_item))
