5b69590f6cc8c0e1dd68b33bb312de59f46a93a5
何小有 <30341875+hekaiyou@users.noreply.github.com>
2022-03-21 14:39:16 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import apis.apis_urls as apis_urls
+import core.api_token as token_urls
+from fastapi import FastAPI
+from core.dependencies import get_settings
+from core.database import create_db_client, close_db_client
+
+settings = get_settings()
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+app.include_router(token_urls.router)
+app.include_router(apis_urls.router)
+
+
+@app.on_event('startup')
+async def startup_event():
+    create_db_client(apis_urls)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    close_db_client()
