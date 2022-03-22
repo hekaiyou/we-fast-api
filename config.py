1fa84573fd3e64df7c6f0f54af313dfefcb28057
何小有 <30341875+hekaiyou@users.noreply.github.com>
2022-03-22 18:29:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_name: str = 'We FastAPI'
     app_version: str = '0.0.1'
+    # 允许跨域请求的源列表
+    allow_origins: list = ['*']
     # MongoDB 连接: 基本信息
     mongo_db_host: str = '127.0.0.1'
     mongo_db_port: int = 27017
@@ -11,6 +13,9 @@ class Settings(BaseSettings):
     # MongoDB 连接: 认证信息
     mongo_db_username: str = ''
     mongo_db_password: str = ''
+    # 微信小程序配置
+    weixin_app_id: str = ''
+    weixin_app_secret: str = ''
 
     class Config:
         env_file = '.env'
