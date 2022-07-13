d5b8ad2321e129df29094a2f89a854c880a77b23
hekaiyou <hekaiyou@qq.com>
2022-07-13 15:54:18 +0800

    完善用户消息接口

--- apis/bases/api_me.py
+++ apis/bases/api_me.py
@@ -23,8 +23,11 @@ router = APIRouter(prefix='/me', )
 async def read_me_info(current_token: TokenData = Depends(get_token_data)):
     user = get_me_user(current_token.user_id)
     user_data = UserGlobal(**user).dict()
-    user_data[
-        'avata_url'] = f'api/bases/user/{current_token.user_id}/avata/open/'
+    if not user['avata']:
+        user_data['avata_url'] = ''
+    else:
+        user_data[
+            'avata_url'] = f'api/bases/user/{current_token.user_id}/avata/open/'
     return user_data
 
 
