307e46bd8e0254dcb968fa9b034a16eed9c8589b
hekaiyou <hekaiyou@qq.com>
2023-01-03 16:15:44 +0800

    自动生成导航栏配置文件

--- apis/apis_urls.py
+++ apis/apis_urls.py
@@ -15,6 +15,7 @@ router_view = APIRouter(
 exclude_dir_path = [
     'apis_urls.py', '__init__.py', '__pycache__', 'templating.py'
 ]
+view_navigation_bars = []
 # 自动查找可用的 API|VIEW 模块并添加路由
 work_path_apis = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 for dir_path in os.listdir(f'{work_path_apis}/apis/'):
@@ -28,3 +29,15 @@ for dir_path in os.listdir(f'{work_path_apis}/apis/'):
             module_name = f'apis.{dir_path}.views.view_url'
             meta_class = importlib.import_module(module_name)
             router_view.include_router(meta_class.router)
+        if os.path.exists(
+                f'{work_path_apis}/apis/{dir_path}/views/view_navigation.py'):
+            module_name = f'apis.{dir_path}.views.view_navigation'
+            meta_class = importlib.import_module(module_name)
+            view_navigation_bars.extend(meta_class.view_navigation_bar)
+view_navigation_bars.sort(key=lambda x: (x['weight']), reverse=True)
+with open(
+        f'{work_path_apis}/apis/bases/views/static/config.js',
+        'w',
+        encoding='UTF-8',
+) as f:
+    f.write(f'var navigationBar = {view_navigation_bars}')
