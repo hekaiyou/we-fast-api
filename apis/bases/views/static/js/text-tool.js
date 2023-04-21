81becc3868528b9d4d0c3985defce087bc3a17e1
hekaiyou <hekaiyou@qq.com>
2023-04-21 23:44:00 +0800

    优化detailsContentText

--- apis/bases/views/static/js/text-tool.js
+++ apis/bases/views/static/js/text-tool.js
@@ -13,3 +13,37 @@ function detectLanguage(text) {
         return 'unknown';
     }
 }
+
+function detailsContentText(text) {
+    let elementWidth = $('#details-content').width();
+    let charWidth = 0;
+    let complete = '';
+    let divisor = 1.6;
+    for (let char of text) {
+        if (/[A-Z]/.test(char)) {
+            // 大写英文字符
+            charWidth += 24.75 / divisor;
+        } else if (/[a-z]/.test(char)) {
+            // 小写英文字符
+            charWidth += 19.7 / divisor;
+        } else if (/\s/.test(char)) {
+            // 空格
+            charWidth += 9.47 / divisor;
+        } else if (/\d/.test(char)) {
+            // 数字
+            charWidth += 18.77 / divisor;
+        } else if (/^[\u4e00-\u9fa5]$/.test(char)) {
+            // 汉字
+            charWidth += 32 / divisor;
+        } else {
+            // 其他
+            charWidth += 32 / divisor;
+        }
+        if (charWidth > elementWidth) {
+            complete += '...';
+            break;
+        }
+        complete += char;
+    }
+    return complete;
+}
