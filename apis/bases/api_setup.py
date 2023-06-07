336dcadffacca98800480ed354e6b26743298c97
hekaiyou <hekaiyou@qq.com>
2023-06-07 14:11:43 +0800

    补充api_setup.py注释

--- apis/bases/api_setup.py
+++ apis/bases/api_setup.py
@@ -15,11 +15,15 @@ router = APIRouter(prefix='/setup', )
 )
 async def read_setup_module_all():
     all_item = []
-    exclude_dir_path = ['apis_urls.py', '__init__.py', '__pycache__']
+    # 在 apis 目录下, 排除非模块目录的其他文件或目录
+    exclude_dir_path = [
+        'apis_urls.py', '__init__.py', '__pycache__', 'templating.py'
+    ]
     work_path = os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     for dir_path in os.listdir(f'{work_path}/apis/'):
         if dir_path not in exclude_dir_path:
+            # 找到模块目录并取出模块名称
             if os.path.exists(f'{work_path}/apis/{dir_path}/config.py'):
                 all_item.append({'name': f'particle-{dir_path}'})
     return NoPaginate(all_item=all_item, total=len(all_item))
@@ -31,6 +35,7 @@ async def read_setup_module_all():
     summary='读取设置',
 )
 async def read_setup(module_name: str):
+    # 通过模块名称 (去掉前面的 particle- 字符串) 获取环境变量
     configs = get_apis_configs(module_name[9:])
     if not configs:
         raise HTTPException(
@@ -38,6 +43,7 @@ async def read_setup(module_name: str):
             detail='找不到模块',
         )
     all_item = []
+    # 通过模块名称获取环境变量的详细信息
     configs_describe = get_apis_configs(f'{module_name[9:]}_describe')
     for config in configs:
         all_item.append({
@@ -55,6 +61,7 @@ async def read_setup(module_name: str):
     summary='更新设置',
 )
 async def update_setup(module_name: str, setup_update: SetupUpdate):
+    # 通过模块名称 (去掉前面的 particle- 字符串) 获取环境变量
     configs = get_apis_configs(module_name[9:])
     if not configs:
         raise HTTPException(
@@ -62,5 +69,6 @@ async def update_setup(module_name: str, setup_update: SetupUpdate):
             detail='找不到模块',
         )
     for key, value in setup_update.setups.items():
+        # 逐个更新当前模块的环境变量
         set_apis_configs(module=module_name[9:], key=key, vlaue=value)
     return {'wid': get_worker_id(setup_update.synced_wids)}
